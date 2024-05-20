fe4e308d2b50b7cca34a04a29809f193dc6229ee
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-05-20 23:01:02 +0500

    Add files via upload

--- js/lecture 1/first.js
+++ js/lecture 1/first.js
@@ -1,5 +1,35 @@
-// let name = "chaudhary hamza";
-// console.log("name"); 
+                                       // In js how to define data
+const rollNumber = 12345                          //mostly use
+let name = "chaudhary hamza";                     //mostly use
+var classNo = "b13"
+college = "superior";
+let changingData;
+// rollNumber = "123 "      //const can't be updated
+name = "hamza"                //let can  update   
+classNo = "b12"               //variable can  update 
+college = "punjab"
+console.log(rollNumber);
+console.table([name , rollNumber , classNo , college , changingData]);
+
+"use strict";                  //when we want to use only modern js code
+// alert(3+3)                  //itc can't work in such way in node js
+
+
+                                    //In Js data types 
+
+let fullName = "string";                  //string
+let num = 123                             //number
+let isLoginIn = false                     //boolean
+let state;                                //undefined
+//number => 2 to power 53 
+// bigint  => use for more then big vlue data restoration then number. we use in stock market
+//string => ""
+//boolean => true/false
+//null => standalone value . for example we use for temperature when server give error its we recive message null not 0
+//undefined => 
+//symbol => unique use in react & for figma tools
+//object => 
+
                                          //    create an object and how to print object key values
 // let  student = {
 //     age : 20,
