a23e69c57b78310801af0beff7ba29273693b21a
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-05-22 01:33:37 +0500

    Add files via upload

--- js/lecture 2/first.js
+++ js/lecture 2/first.js
@@ -1,131 +1,147 @@
-                                            //   lecture 2
-                                            // Arithmetic operators
-// let a = 5;
-// let b = 2;
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(a % b);
-// console.log(a ** b);
-// console.log(a ++);
-// console.log(a --);
-// console.log(++ a);
-// console.log(-- a);
-                                            // Assignment operators
-// let a = 5;
-// let b = 2;
-// a += 1;
-// a = a +1;
-// console.log (a );
-// console.log(c = a + b) ;
-// console.log (a += b);
-// console.log (a += 1);
-// console.log (a *= 1);
-// console.log (a -= 1);
-// console.log (a /= 2);
-// console.log (a **= 1);
-// console.log (a %= 2);
-                                            // comparison operators
-// the Ans is true or false
-// let a = 5;
-// let b = 2;
-// console.log(a == b);
-// console.log(a != b);
+                                       // ********************In js how to define data******************************
 
-// in this its also check data type
-// let a = 5;
-// let b = "5";
-// console.log(a === b);
-// console.log(a !== b);
 
-// let a = 5;
-// let b = 2;
-// console.log(a > b);
-// console.log(a >= b);
-// console.log(a < b);
-// console.log(a <= b);
-                                                              // logical and operators
-// let a = 5;
-// let b = 2;
-// let cond1 = a === 5;
-// let cond2 = a > b;
-// console.log("cond1 && cond2 =", cond1 && cond2);
-                                                                 // logical or operators
-// let a = 5;
-// let b = 2;
-// let cond1 = a === 5;
-// let cond2 = a < b;
-// console.log("cond1 || cond2 =", cond1 || cond2);
-                                                                   // logical not operators
-// let a = 5;
-// let b = 2;
-// let cond1 = a === 5;
-// let cond2 = a < b;
-// console.log("cond1 !|| cond2 =", !(cond1 || cond2));
+const rollNumber = 12345                          //mostly use
+let name = "chaudhary hamza";                     //mostly use
+var classNo = "b13"
+college = "superior";
+let changingData;
+// rollNumber = "123 "      //const can't be updated
+name = "hamza"                //let can  update   
+classNo = "b12"               //variable can  update 
+college = "punjab"
+console.log(rollNumber);
+console.table([name , rollNumber , classNo , college , changingData]);
 
-                                                                 // conditional statements   
-                                                                    //  if statement 
-// let a ="5";
-// if (a = 5 ){
-//     console.log("hello!") ;
-// }
-                                                                    //  if else statement    
+"use strict";                  //when we want to use only modern js code
+// alert(3+3)                  //itc can't work in such way in node js
 
-// let a = 3 ;
-// if (a > 5 ){
-//     console.log("yes") ;
-// }
-// else {
-//     console.log("no") ;
-// }
-                                                                    //  else if statement  
-// let age = "4" ;
-// if (age > "5" ){
-//     console.log("yes") ;
-// }
-// else if (age === "3" ){
-//     console.log("equal") ;
-// }
-// else if (age === "2" ){
-//     console.log("equal") ;
-// }
-// else {
-//     console.log("no") ;
-// }
-                                                                    //  ternary operators
-// let age = 15;
-// let result = (age >= 18 ? "adult" : "not adult")
-//    console.log (result);
-                                                                    //   task 1
-// let student = age = prompt("enter a number");
-// let result = (age %5  === 0 ? "it is a multiple of 5": "not multiple of 5")
-//    console.log (result);   
 
-// let  age = prompt("enter a number:");
-// if (age %5 === 0){
-//    console.log("multiple of 5");
+                                    //***************************** In Js data types *****************************
+
+
+
+let fullName = "string";                  //string
+let numbers = 123                             //number
+let isLoginIn = false                     //boolean
+let state;                                //undefined
+//number => 2 to power 53 
+// bigint  => use for more then big vlue data restoration then number. we use in stock market
+//string => ""
+//boolean => true/false
+//null => standalone value . for example we use for temperature when server give error its we recive message null not 0
+//undefined =>  
+//symbol => unique use in react & for figma tools
+//object => 
+
+
+                                            //***************   convertion of data types    ***********************
+
+
+
+// let first = 33
+// let second = "33"
+// let third= "sore"
+// let four = null
+// let five = undefined
+// let sixIsBoolean = true;
+// let sevenIsNumber = 1 ;
+// let eightIsNumber = 0 ;
+// let nineIsNumber = "" ;
+// let tenIsNumber = "hamza" ;
+// console.log(typeof first); 
+// console.log(typeof second);
+// console.log(typeof third);
+// console.log(typeof four);
+// console.log(typeof five);
+
+//                                          //convert number into string
+// let firstNumber = String(first);
+// console.log(firstNumber);
+// console.log(typeof firstNumber);
+
+//                                          //convert string into number
+// let secondNumber = Number(second);
+// console.log(typeof secondNumber);
+// console.log(secondNumber);
+
+// let thirdNumber = Number(third);
+// console.log(typeof thirdNumber);
+// console.log(thirdNumber); 
+
+//                                          //convert null into number
+// let fourNumber = Number( four);
+// console.log(typeof fourNumber);
+// console.log(fourNumber);
+
+//                                          //convert undefine into number
+// let fiveNumber = Number(five);
+// console.log(typeof fiveNumber);
+// console.log(fiveNumber);
+
+//                                          //convert boolean into number
+// let sixNumber = Number(sixIsBoolean);
+// console.log(typeof sixNumber);
+// console.log(sixNumber);
+
+//                                          //convert number into boolean
+//   // 1 into boolean
+// let sevenNumber = Boolean(sevenIsNumber);
+// console.log(typeof sevenNumber);
+// console.log(sevenNumber);
+//   // 0 into boolean
+// let eightNumber = Boolean(eightIsNumber);
+// console.log(typeof eightNumber);
+// console.log(eightNumber);
+//   // "" into boolean
+// let nineNumber = Boolean(nineIsNumber);
+// console.log(typeof nineNumber);
+// console.log(nineNumber);
+//   // "hamza" into boolean
+// let tenNumber = Boolean(tenIsNumber);
+// console.log(typeof tenNumber);
+// console.log(tenNumber);
+
+
+//                                        *************************************************
+
+
+
+
+                                         //   ********** create an object and how to print object key values
+// let  student = {
+//     age : 20,
+//     class: 6,
+//     name: "hamza",
+//     section:"b",
+// };
+// console.log(student);
+// console.log (typeof student);
+// console.log (typeof student.age);
+// console.log (typeof student["class"]);
+                                            //  lecture tasks 1
+// const product = {
+//     type : "pen",
+//     name : "standard ct ball pen",
+//     Rating : 4,
+//     isdeal : true,
+//     offer : 5,
+//     price: 270,
 // }
-// else{
-//    console.log("not multiple of 5");
-// }
-                                                                    //   task 2
-// let  age = prompt("enter a number:");
-// if ( age <= 49){
-//    console.log ("F");
-// }
-// else if ( age <= 59){
-//    console.log ("d");
-// }
-// else if ( age <= 69){
-//    console.log ("c");
-// }
-// else if ( age <= 89){
-//    console.log ("b");
-// }
-// else if ( age <= 100){
-//    console.log ("a");
-// }
-// else {
-//    console.log ("wrong number");
-// }
+// console.log (product,);
+// console.log (typeof product,);
+// console.log (typeof product.isdeal);
+// console.log (typeof product["isdeal"]);
+                                            //  lecture tasks 2
+// const profile ={
+//     username:"hamza",
+//     isfollow:true,
+//     posts:195,
+//     followers:595,
+//     following:4,
+// };
+// console.log (profile,);
+// console.log (typeof profile,);
+// console.log (typeof profile.isdeal);
+// console.log (typeof profile["isdeal"]);
+                                            //  lecture 1 end
