e337001ee7894fbc2779fc855ace2dd2134d1aa9
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-05-22 01:32:27 +0500

    Add files via upload

--- js/lecture 1/first.js
+++ js/lecture 1/first.js
@@ -1,4 +1,6 @@
-                                       // In js how to define data
+                                       // ********************In js how to define data******************************
+
+
 const rollNumber = 12345                          //mostly use
 let name = "chaudhary hamza";                     //mostly use
 var classNo = "b13"
@@ -15,10 +17,12 @@ console.table([name , rollNumber , classNo , college , changingData]);
 // alert(3+3)                  //itc can't work in such way in node js
 
 
-                                    //In Js data types 
+                                    //***************************** In Js data types *****************************
+
+
 
 let fullName = "string";                  //string
-let num = 123                             //number
+let numbers = 123                             //number
 let isLoginIn = false                     //boolean
 let state;                                //undefined
 //number => 2 to power 53 
@@ -26,11 +30,85 @@ let state;                                //undefined
 //string => ""
 //boolean => true/false
 //null => standalone value . for example we use for temperature when server give error its we recive message null not 0
-//undefined => 
+//undefined =>  
 //symbol => unique use in react & for figma tools
 //object => 
 
-                                         //    create an object and how to print object key values
+
+                                            //***************   convertion of data types    ***********************
+
+
+
+// let first = 33
+// let second = "33"
+// let third= "sore"
+// let four = null
+// let five = undefined
+// let sixIsBoolean = true;
+// let sevenIsNumber = 1 ;
+// let eightIsNumber = 0 ;
+// let nineIsNumber = "" ;
+// let tenIsNumber = "hamza" ;
+// console.log(typeof first); 
+// console.log(typeof second);
+// console.log(typeof third);
+// console.log(typeof four);
+// console.log(typeof five);
+
+//                                          //convert number into string
+// let firstNumber = String(first);
+// console.log(firstNumber);
+// console.log(typeof firstNumber);
+
+//                                          //convert string into number
+// let secondNumber = Number(second);
+// console.log(typeof secondNumber);
+// console.log(secondNumber);
+
+// let thirdNumber = Number(third);
+// console.log(typeof thirdNumber);
+// console.log(thirdNumber); 
+
+//                                          //convert null into number
+// let fourNumber = Number( four);
+// console.log(typeof fourNumber);
+// console.log(fourNumber);
+
+//                                          //convert undefine into number
+// let fiveNumber = Number(five);
+// console.log(typeof fiveNumber);
+// console.log(fiveNumber);
+
+//                                          //convert boolean into number
+// let sixNumber = Number(sixIsBoolean);
+// console.log(typeof sixNumber);
+// console.log(sixNumber);
+
+//                                          //convert number into boolean
+//   // 1 into boolean
+// let sevenNumber = Boolean(sevenIsNumber);
+// console.log(typeof sevenNumber);
+// console.log(sevenNumber);
+//   // 0 into boolean
+// let eightNumber = Boolean(eightIsNumber);
+// console.log(typeof eightNumber);
+// console.log(eightNumber);
+//   // "" into boolean
+// let nineNumber = Boolean(nineIsNumber);
+// console.log(typeof nineNumber);
+// console.log(nineNumber);
+//   // "hamza" into boolean
+// let tenNumber = Boolean(tenIsNumber);
+// console.log(typeof tenNumber);
+// console.log(tenNumber);
+
+
+//                                        *************************************************
+
+
+
+
+                                         //   ********** create an object and how to print object key values
 // let  student = {
 //     age : 20,
 //     class: 6,
