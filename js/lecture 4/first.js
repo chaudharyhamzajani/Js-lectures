25a7317920c25de6123259d0b945b7c12a373c7d
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-05-24 03:52:02 +0500

    Add files via upload

--- js/lecture 4/first.js
+++ js/lecture 4/first.js
@@ -3,19 +3,19 @@
 // console.log(marks);
 
 // let booksName = ["urdu","math","science","computer" , "eng"];
-                                                  //array length
+//                                                 //   array length
 // console.log(booksName .length);
 // console.log(booksName);
 // console.log(typeof booksName);
 // booksName[0] = "physics"
-                                               //array indices
+//                                                //array indices
 // console.log(booksName[0]);
                                                //arrays using for loop
 // let booksName = ["urdu","math","science","computer" , "eng"];
 // for (i = 0 ; i < booksName.length; i ++){
 //     console.log(booksName[i]);
-// }
-                                               //arrays using for loop
+// };
+                                               //arrays using for-of loop
 // let booksName = ["urdu","math","science","computer" , "eng"];
 // for(let val of booksName){
 //     console.log(val);
@@ -25,14 +25,14 @@
 // for (let val of citys){
 //     console.log (val.toUpperCase());
 // }
-                                               //task 1 sum of arrays values
+//                                                //task 1 sum of arrays values
 // let marks = [85,97,44,37,76,60];
 // let sum = 0
 //  for(let val of marks){
 //     sum = sum + val ;
-//     avg = sum / marks.length
+// //     avg = sum / marks.length
 //  }  
-//     console.log(avg);
+//     console.log(sum);
                                                    //task 1 sum of arrays values
 // let prices = [250 ,645,300, 900 ,50 ];
 // let i= 0
@@ -65,7 +65,37 @@
 // let marks = [10 ,20 , 30 , 40 ];
 // console.log(marks);
 // console.log(marks.toString());
-           // concat()
+//           //include()
+// let marks = [10 ,20 , 30 , 40 ];                //this value exist or not
+// console.log(marks);
+// console.log(marks.includes(12));
+//           //indexof()
+// let marks = [10 ,20 , 30 , 40 ];                
+// console.log(marks);
+// console.log(marks.indexOf(12));                 //if value exist it's print its index otherwise its print -1
+// console.log(marks.indexOf(30));
+//           //join()
+// let marks = [10 ,20 , 30 , 40 ];                
+// console.log(marks);
+// console.log(marks.join());                 //its change the value form array to string
+// console.log(typeof marks);
+//           //flat()
+// let marks = [10 ,20 , 30 , 40 , [1 , 2 , [3 , 4]]];                
+// console.log(marks);
+// console.log(marks.flat(2));                 //the value tell about the depth of  array 
+// console.log(typeof marks);
+//           //Array.isArray & Array.from() 
+// let marks = "hamza"              
+// console.log(typeof marks);
+// console.log(Array.isArray("hamza"));                 //its change the value form string to array
+// let change = console.log(Array.from("hamza"));
+// console.log(typeof change);
+//           // Array.of()
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3));                 //multiple varibles change into array
+        //    concat()
 // let myEntery = ["hamza", "zubair" ];
 // let seniorEntery = ["umar", "abbas"];
 // let juniorEntery = ["saif", "tanveer"];
@@ -86,14 +116,14 @@
 // console.log(myEntery);
             //splice()
 // let myEntery = ["hamza", "zubair","saif","tanveer", "faisal","iqrar" ];
-// console.log(myEntery);
-// //replace
-// // console.log(myEntery.splice(2 ,2,"umar","abbas"));
-// //add 
-// // console.log(myEntery.splice(2 ,0 ,"umar","abbas"));
-// //delete
-// // console.log(myEntery.splice(2 ,2,));
-// // console.log(myEntery.splice(3 ));
+// console.log(myEntery);                             
+// replace
+// console.log(myEntery.splice(2 ,2,"umar","abbas"));                 
+// add                                        //first value pass where to start.2nd value how much delete.3rd which value add
+// console.log(myEntery.splice(2 ,0 ,"umar","abbas"));
+// // delete
+// console.log(myEntery.splice(2 ,2,));
+// console.log(myEntery.splice(3 ));
 // console.log(myEntery.splice( ));
 // console.log(myEntery);
                                                    //task 2 
@@ -106,6 +136,23 @@
 // console.log(companies.splice(5,0,"Amazon"));
 // console.log(companies);
 
- 
-
-   
+                                           //***********objects***********
+   let mysym = Symbol("key01")
+let jsuser = {
+        name : "hamza",
+        age  : 20, 
+        email : "www.google.co",
+        isfollow : true,
+        [mysym] : "key01",
+        "full name" : " name"
+   };
+   console.log(jsuser.age);
+   console.log(jsuser["age"]);
+   console.log(jsuser["full name"]);
+   console.log(jsuser[mysym]);
+  
+   jsuser.age = 12
+   console.log(jsuser);
+   Object.freeze(jsuser);
+   jsuser.age = 15
+   console.log(jsuser);
