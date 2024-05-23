69302d7f0329425328f079460ad30cca21f2cae2
Hamza <161723182+chaudharyhamzajani@users.noreply.github.com>
2024-05-24 03:50:10 +0500

    Add files via upload

--- js/lecture 2/first.js
+++ js/lecture 2/first.js
@@ -20,72 +20,72 @@
 
 // console.log("2" > 1);
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null > 0);                      //false
+// console.log(null == 0);                      //false
+// console.log(null >= 0);                      //true
 
-// console.log(undefined == 0);
-// console.log(undefined > 0);
-// console.log(undefined < 0);
+// console.log(undefined == 0);                      //false
+// console.log(undefined > 0);                      //false
+// console.log(undefined < 0);                      //false
               // *****bad code practise end******
               //js type  static or dynamic 
-              //memories types  
-//first Primitive
-//7 types : String , Number, Boolean , null , undefined, Symbol, BigInt
+              //               *********memories types*************  
+// first Primitive
+// 7 types : String , Number, Boolean , null , undefined, Symbol, BigInt
 
-//examples
-// 1) string
-const str = "string";
-// 2) Number
-const num = 12
-// 3) Boolean
-const booleanValue = true;
-// 4) null
-let temp = null;
-// 5) undefine
-let undefineValue;
-// 6) symbol
-let id = Symbol(123);
-let newId = Symbol(123);
-console.log(id === newId);
-// 7) bigInt
-let newValue = 12333333333n
-console.log(typeof newValue);
+// //examples
+// // 1) string
+// const str = "string";
+// // 2) Number
+// const num = 12
+// // 3) Boolean
+// const booleanValue = true;
+// // 4) null
+// let temp = null;
+// // 5) undefine
+// let undefineValue;
+// // 6) symbol
+// let id = Symbol(123);
+// let newId = Symbol(123);
+// console.log(id === newId);
+// // 7) bigInt
+// let newValue = 12333333333n
+// console.log(typeof newValue);
 
-//second non-Primitive or Reference type
-//Arrays , object , function
-// example
-//arrays
-let heros = ["hamza" , "umar" , "ali"];
-//object
-let myobj = {
-  name : "hamza",
-  age : 20,
-  student : true,
-}
-//function
-let myfunction = function(){
-  console.log("hello");
-};
-console.log(typeof id);
+// //second non-Primitive or Reference type
+// //Arrays , object , function
+// // example
+// //arrays
+// let heros = ["hamza" , "umar" , "ali"];
+// //object
+// let myobj = {
+//   name : "hamza",
+//   age : 20,
+//   student : true,
+// }
+// //function
+// let myfunction = function(){
+//   console.log("hello");
+// };
+// console.log(typeof id);
                                       //  *********************************************
-// stack(permitive) , heap (non-permitive)         //copy of reference value
-let valueOne = "valueone.com";
-let valueTwo = valueOne;
-valueTwo = "change value"
-console.log(valueOne);
-console.log(valueTwo);
+// // stack(permitive) , heap (non-permitive)         //copy of reference value
+// let valueOne = "valueone.com";
+// let valueTwo = valueOne;
+// valueTwo = "change value"
+// console.log(valueOne);
+// console.log(valueTwo);
 
-//example of heap (non-permitive)           //copy of original value
-let valueThree = {
-  name : "hamza",
-  age : 20,
-  student : true, 
-};
-let valueFour = valueThree;
-valueFour.age = 21
-console.log(valueFour);
-console.log(valueThree);
+// //example of heap (non-permitive)           //copy of original value
+// let valueThree = {
+//   name : "hamza",
+//   age : 20,
+//   student : true, 
+// };
+// let valueFour = valueThree;
+// valueFour.age = 21
+// console.log(valueFour);
+// console.log(valueThree);
 
                                             // Arithmetic operators
 // let a = 5;
